d1c6b1bfe931488589560ad66b700518f0e5207a
khurram0334 <khurram.shahzad.nbc@gmail.com>
2022-01-25 12:47:56 +0500

    first commit

---
+++ code_file.py
@@ -0,0 +1,12 @@
+from re import L
+
+
+name = "khuram "
+print (len(name))
+print (name [-5])
+print (type(name))
+if  bool(name) == True:
+    print ("gooof")
+
+
+print ("my sale man ages are %(atif)s , %(amjad)s, %(sohail)s" %{"atif": 40, "amjad": 20, "sohail": 30} )
---
+++ print_function.py
@@ -0,0 +1,5 @@
+# print is function we can use single "" and double""" , inside quotes called string
+print ("double collen ")
+print('single qute')
+print ("u can use 'single qute' in double qutes")
+#print ('khurram 'shahza') u can not use single qute inside single quote and double quotetocu
