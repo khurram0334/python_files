727a0fcea58829d33de41323a990a951019ac7bb
khurram0334 <khurram.shahzad.nbc@gmail.com>
2022-01-27 16:44:47 +0500

    new lines

--- new_sum.py
+++ new_sum.py
@@ -1,6 +1,12 @@
-b = 0
+s = 0
+r = 0
 
-for a in range(10):
-    print (b)
-    b = a + 1
+for c in range(10):
+    
+    print ("current number ", c, 'prvious number ', s, "sum result =", r)
+
+    s = c
+    r = s + c
+    r = r + 1
+     
     
